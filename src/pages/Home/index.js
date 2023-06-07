0803a744dca90fadcc55d0428f0708749b8f6dd3
alessandrofeitoza <alessandrofeitoza01@gmail.com>
2023-06-06 21:55:19 -0300

    Loja online

---
+++ src/pages/Home/index.js
@@ -0,0 +1,15 @@
+import Footer from "../../components/Footer";
+import Menu from "../../components/Menu";
+import Navbar from "../../components/Navbar";
+
+export default function Home() {
+    return (
+        <div>
+            <Navbar/>
+
+            <Menu/>
+
+            <Footer/>
+        </div>
+    )
+}
