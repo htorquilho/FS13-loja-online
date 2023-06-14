9c7b6b471bc0ac439f88358293a577d0a3cfe395
alessandrofeitoza <alessandrofeitoza01@gmail.com>
2023-06-13 22:51:23 -0300

    Aula 28 - parte 1

--- src/components/FeaturedCollections/index.js
+++ src/components/FeaturedCollections/index.js
@@ -1,16 +1,38 @@
-import { Grid } from "@mui/material";
+import { Grid, Button } from "@mui/material";
+import "./styles.scss";
+import tenis from "./img/tenis.png";
 
 export default function FeaturedCollections() {
+    const Card = (props) => {
+        return (
+            <Grid item xs={4}>
+                <div className="card">
+                    <div>
+                        {props.discount}
+                        {props.discount && "%"} 
+                        
+                        <br/>
+                        {props.text} <br/>
+
+                        <Button variant="contained">Comprar</Button>
+                    </div>
+
+                    <div>
+                        <img src={props.image}/>
+                    </div>
+                </div>
+            </Grid>
+        )
+    }
+
     return (
-        <div>
+        <div className="featured-collections">
             Coleções em Destaque
 
-            <Grid container>
-                <Grid item xs={4}> A </Grid>
-                
-                <Grid item xs={4}> B </Grid>
-                
-                <Grid item xs={4}> C </Grid>
+            <Grid container spacing={2}>
+                <Card text="Camisetas" discount={30} image={tenis}/>
+                <Card text="Tenis" image={tenis}/>
+                <Card text="Headsets" discount={20} image={tenis}/>
             </Grid>
         </div>
     )
