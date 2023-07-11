d5a6d0fad369830456dd018ca63246bfe84e6280
andrebarrosa <andrebarros_@hotmail.com>
2023-07-10 21:23:27 -0300

    Merge branch 'checkform' of https://github.com/htorquilho/fs13-loja-onlineasdad

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Fragment } from "react";
 import { Grid } from "@mui/material";
 import digitalstore from "../Footer/img/digitalstore-icon.svg";
 import facebook from "../Footer/img/facebook-icon.svg";
@@ -11,8 +10,8 @@ export default function Footer() {
   return (
   <div className="footer">
         <Grid container className="footer-grid">
-          <Grid item>
-              <div className="logo-div">
+          <Grid>
+              <div className="logomarca">
                 <img className="logo-img" src={digitalstore} alt="Icone da Digital Store"/>
                 <div className="titulo-loja">Digital Store</div>
               </div> 
@@ -23,7 +22,7 @@ export default function Footer() {
               <img src={instagram} className="insta-icon" alt="ícone do Instagram"/>
               <img src={twitter} className="twit-icon" alt="ícone do Twitter"/>
           </Grid>
-          <Grid item>
+          <Grid>
               <h4 className="titulo-info">Informação</h4>
 
               <div className="div-info">
@@ -36,7 +35,7 @@ export default function Footer() {
               </div>  
           </Grid>
 
-          <Grid item>
+          <Grid>
               <h4 className="titulo-categorias">Categorias</h4>
 
               <div className="div-categorias">    
@@ -48,7 +47,7 @@ export default function Footer() {
               </div> 
           </Grid>
 
-          <Grid item>
+          <Grid>
               <h4 className="titulo-contato">Contato</h4>
               <p className="p-endereco">Av. Santos Dumont, 1510 - 1 andar - Aldeota, Fortaleza - CE, 60150-161</p>
               <p className="p-telefone">(85) 3051-3411</p>  
---
+++ src/pages/Products/index.js
@@ -0,0 +1,17 @@
+import Footer from "../../components/Footer";
+import Menu from "../../components/Menu";
+import Navbar from "../../components/Navbar";
+import CheckForm from "../../components/CheckForm";
+
+export default function Products() {
+    return (
+        <div>
+            <Navbar/>
+            <Menu/>
+
+            <CheckForm/>
+            
+            <Footer/>
+        </div>
+    )
+}
